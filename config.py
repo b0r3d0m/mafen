74e60c9ace6f5bedbca0baf2e04dab553a576b60
Nikita Trophimov <n.trophimov@tools4brokers.com>
2017-02-18 12:27:29 +0400

    Fix indentation

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import ConfigParser
 
 
-class Config:
-  def load(self, filename):
-    config = ConfigParser.SafeConfigParser()
-    config.read(filename)
+class Config(object):
+    def load(self, filename):
+        config = ConfigParser.SafeConfigParser()
+        config.read(filename)
 
-    self.mafen_host = config.get('mafen', 'host')
-    self.mafen_port = int(config.get('mafen', 'port'))
+        self.mafen_host = config.get('mafen', 'host')
+        self.mafen_port = int(config.get('mafen', 'port'))
 
-    self.auth_host = config.get('auth', 'host')
-    self.auth_port = int(config.get('auth', 'port'))
-    self.cert_path = config.get('auth', 'cert_path')
+        self.auth_host = config.get('auth', 'host')
+        self.auth_port = int(config.get('auth', 'port'))
+        self.cert_path = config.get('auth', 'cert_path')
 
-    self.game_host = config.get('game', 'host')
-    self.game_port = int(config.get('game', 'port'))
+        self.game_host = config.get('game', 'host')
+        self.game_port = int(config.get('game', 'port'))
