d87b91fb94d27678004fd4415eadca3162e9820e
Nikita Trophimov <n.trophimov@tools4brokers.com>
2017-02-20 12:39:28 +0400

    Move ws-related code to the corresponding service

--- web/client/app.js
+++ web/client/app.js
@@ -11,20 +11,117 @@ require('angular-ui-bootstrap/dist/ui-bootstrap-tpls.js');
 require('alertify.js/dist/js/ngAlertify.js');
 var jsSHA256 = require('js-sha256/build/sha256.min.js');
 
-// TODO
-var loggedIn = false;
-
 var app = angular.module('app', ['ngAlertify', 'ngRoute', 'ui.bootstrap'])
+.service('mafenSession', function($rootScope, $uibModal, alertify, $timeout) {
+  'ngInject';
+
+  var that = this;
+
+  this.reset = function() {
+    that.loggedIn = false;
+    that.characters = [];
+    that.items = [];
+    that.meters = {};
+    that.attrs = {};
+  };
+
+  var onmessage = function(message) {
+    var msg = JSON.parse(message.data);
+
+    if (msg.action === 'connect') {
+      if (msg.success) {
+        $uibModal.open({
+          ariaLabelledBy: 'charlist-modal-title',
+          ariaDescribedBy: 'charlist-modal-body',
+          templateUrl: 'charlist.html',
+          controller: 'CharacterListModalCtrl',
+          size: 'lg'
+        });
+        that.loggedIn = true;
+      } else {
+        alertify.error('Authentication failed');
+      }
+    } else if (msg.action === 'character') {
+      that.characters.push(msg.name);
+    } else if (msg.action === 'item') {
+      that.items.push(msg);
+    } else if (msg.action === 'destroy') {
+      that.items = that.items.filter(function(item) {
+        return item.id !== msg.id;
+      });
+      delete that.meters[msg.id];
+    } else if (msg.action === 'attr') {
+      that.attrs = msg.attrs;
+    } else if (msg.action === 'meter') {
+      that.meters[msg.id] = msg.meter;
+    } else {
+      // TODO
+    }
+    $rootScope.$apply();
+  };
+
+  this.waitForConnection = function(callback, interval) {
+    if (that.ws.readyState === 1) { // OPEN
+      callback();
+    } else {
+      $timeout(function() {
+        that.waitForConnection(callback, interval);
+      }, interval);
+    }
+  };
+
+  this.connect = function(addr) {
+    that.ws = new WebSocket(addr);
+    that.ws.onmessage = onmessage;
+  };
+
+  this.send = function(data) {
+    // To avoid "Error: Failed to execute 'send' on 'WebSocket': Still in CONNECTING state"
+    that.waitForConnection(function() {
+      that.ws.send(JSON.stringify(data));
+    }, 1000);
+  };
+
+  this.close = function() {
+    that.ws.close();
+  };
+
+  this.getTotalMW = function() {
+    var total = 0;
+    for (var i = 0; i < that.items.length; ++i) {
+      var item = that.items[i];
+      if (item.info.curio && item.study) {
+        total += item.info.mw;
+      }
+    }
+    return total;
+  };
+
+  this.getProgress = function(id) {
+    var progress = '';
+    for (var i = 0; i < that.items.length; ++i) {
+      var item = that.items[i];
+      if (item.id === id) {
+        var meter = that.meters[id];
+        if (meter !== undefined) {
+          progress = meter + '%';
+        }
+        break;
+      }
+    }
+    return progress;
+  };
+})
 .config(function($routeProvider, $locationProvider) {
   'ngInject';
 
-  var checkLoggedin = function($q, $location) {
+  var checkLoggedin = function($q, $location, mafenSession) {
     'ngInject';
 
     // TODO: remove defer
     var deferred = $q.defer();
 
-    if (loggedIn) {
+    if (mafenSession.loggedIn) {
       deferred.resolve();
     } else {
       deferred.reject();
@@ -50,114 +147,48 @@ var app = angular.module('app', ['ngAlertify', 'ngRoute', 'ui.bootstrap'])
       redirectTo: '/'
     });
 })
-.run(function($rootScope, $location, $uibModal, alertify) {
+.run(function($rootScope, $location, mafenSession) {
   'ngInject';
 
   $rootScope.logout = function() {
-    $rootScope.ws.close();
-    loggedIn = false;
+    mafenSession.close();
+    mafenSession.loggedIn = false;
     $location.url('/login');
-  };
-
-  // TODO: Move the following code to the service
-
-  $rootScope.characters = [];
-  $rootScope.items = [];
-  $rootScope.meters = {};
-  $rootScope.attrs = {};
-
-  $rootScope.ws = new WebSocket('ws://127.0.0.1:8000');
-
-  $rootScope.ws.onmessage = function(message) {
-    console.log(message.data); // TODO: rm
-
-    var msg = JSON.parse(message.data);
-
-    if (msg.action === 'connect') {
-      if (msg.success) {
-        $uibModal.open({
-          ariaLabelledBy: 'charlist-modal-title',
-          ariaDescribedBy: 'charlist-modal-body',
-          templateUrl: 'charlist.html',
-          controller: 'CharacterListModalCtrl',
-          size: 'lg'
-        });
-        loggedIn = true;
-      } else {
-        alertify.error('Authentication failed');
-      }
-    } else if (msg.action === 'character') {
-      $rootScope.characters.push(msg.name);
-    } else if (msg.action === 'item') {
-      $rootScope.items.push(msg);
-    } else if (msg.action === 'destroy') {
-      $rootScope.items = $rootScope.items.filter(function(item) {
-        return item.id !== msg.id;
-      });
-      delete $rootScope.meters[msg.id];
-    } else if (msg.action === 'attr') {
-      $rootScope.attrs = msg.attrs;
-    } else if (msg.action === 'meter') {
-      $rootScope.meters[msg.id] = msg.meter;
-    } else {
-      // TODO
-    }
-    $rootScope.$apply();
-  };
-
-  $rootScope.getTotalMW = function() {
-    var total = 0;
-    for (var i = 0; i < $rootScope.items.length; ++i) {
-      var item = $rootScope.items[i];
-      if (item.info.curio && item.study) {
-        total += item.info.mw;
-      }
-    }
-    return total;
-  };
-
-  $rootScope.getProgress = function(id) {
-    var progress = '';
-    for (var i = 0; i < $rootScope.items.length; ++i) {
-      var item = $rootScope.items[i];
-      if (item.id === id) {
-        var meter = $rootScope.meters[id];
-        if (meter !== undefined) {
-          progress = meter + '%';
-        }
-        break;
-      }
-    }
-    return progress;
   };
 });
 
-app.controller('LoginCtrl', function($rootScope, $scope, $location) {
+app.controller('LoginCtrl', function($scope, mafenSession) {
   'ngInject';
+
+  $scope.mafenSession = mafenSession;
+  $scope.mafenSession.reset();
 
   $scope.user = {};
 
   $scope.login = function() {
-    $rootScope.ws.send(JSON.stringify({
+    $scope.mafenSession.connect('ws://127.0.0.1:8000');
+    $scope.mafenSession.send({
       action: 'connect',
       data: {
         username: $scope.user.username,
         password: jsSHA256.sha256($scope.user.password)
       }
-    }));
+    });
   };
 });
 
-app.controller('MainCtrl', function($rootScope, $scope) {
+app.controller('MainCtrl', function($scope, mafenSession) {
   'ngInject';
 
+  $scope.mafenSession = mafenSession;
+
   $scope.transferItem = function(id) {
-    $rootScope.ws.send(JSON.stringify({
+    $scope.mafenSession.send({
       action: 'transfer',
       data: {
         id: id
       }
-    }));
+    });
   };
 
   $scope.minutesToHoursMinutes = function(totalMins) {
@@ -167,16 +198,18 @@ app.controller('MainCtrl', function($rootScope, $scope) {
   };
 });
 
-app.controller('CharacterListModalCtrl', function($rootScope, $scope, $location, $uibModalInstance) {
+app.controller('CharacterListModalCtrl', function($scope, $location, $uibModalInstance, mafenSession) {
   'ngInject';
 
+  $scope.mafenSession = mafenSession;
+
   $scope.chooseCharacter = function(character) {
-    $rootScope.ws.send(JSON.stringify({
+    $scope.mafenSession.send({
       action: 'play',
       data: {
         char_name: character
       }
-    }));
+    });
     $scope.close();
     $location.url('/');
   };
