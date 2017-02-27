50472b786108d060d907ef743c822e9827971596
ldagner <through.a.haze@gmail.com>
2017-02-28 00:25:22 +0200

    "remember me" option - using cookies for autoconnect

--- web/client/scripts/controllers/LoginCtrl.js
+++ web/client/scripts/controllers/LoginCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app').controller('LoginCtrl', function($scope, $uibModal, mafenSession, alertify, PATHS) {
+angular.module('app').controller('LoginCtrl', function($scope, $uibModal, mafenSession, alertify, PATHS, $cookies) {
   'ngInject';
 
   $scope.mafenSession = mafenSession;
@@ -8,10 +8,10 @@ angular.module('app').controller('LoginCtrl', function($scope, $uibModal, mafenS
   $scope.user = {};
 
   $scope.login = function() {
-    $scope.mafenSession.reset();
-    $scope.mafenSession.connect('ws://mafen.club:8000');
-    $scope.loginPromise = $scope.mafenSession.login($scope.user.username, $scope.user.password);
-    $scope.loginPromise.then(function() {
+    var loginPromise = createLoginConnect($scope.user.username, $scope.user.password);
+    loginPromise.then(function(e) {
+      if ($scope.rememberMe) setCookies();
+
       $uibModal.open({
         ariaLabelledBy: 'charlist-modal-title',
         ariaDescribedBy: 'charlist-modal-body',
@@ -22,4 +22,45 @@ angular.module('app').controller('LoginCtrl', function($scope, $uibModal, mafenS
       alertify.error('Authentication failed');
     });
   };
+
+  function setCookies() {
+    var user = {
+      username: $scope.user.username,
+      password: $scope.user.password
+    }
+
+    var expireDate = new Date();
+    expireDate.setFullYear(expireDate.getFullYear() + 1);
+
+    $cookies.putObject('user', user, {'expires': expireDate});
+  }
+
+  //attempt to auto login the user
+  function autoLogin() {
+    var currentUser = $cookies.getObject('user');
+
+    if (currentUser && currentUser != "") {
+      var loginPromise = createLoginConnect(currentUser.username, currentUser.password)
+
+      loginPromise.then(function() {
+        $uibModal.open({
+          ariaLabelledBy: 'charlist-modal-title',
+          ariaDescribedBy: 'charlist-modal-body',
+          templateUrl: PATHS.views + 'charlist.html',
+          controller: 'CharListCtrl'
+        });
+      }, function() {
+        alertify.error('Oops! Something went wrong');
+      })
+    }
+  };
+
+  function createLoginConnect(username, password) {
+    $scope.mafenSession.reset();
+    $scope.mafenSession.connect('ws://localhost:8000');
+    $scope.loginPromise = $scope.mafenSession.login(username, password);
+    return $scope.loginPromise;
+  };
+
+  autoLogin();
 });
