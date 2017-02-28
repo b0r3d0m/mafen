21759eb9a86efee9f75366d0f3cd1ccbe06ad2b6
pil-a <pil-a@yandex.ru>
2017-02-28 15:25:48 +0300

    remove large ifelse block, change all on functions in new module

---
+++ web/client/scripts/services/messageActions.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var messageActions = {
+  init: function($rootScope) {
+    this.findFirstWithProp = $rootScope.findFirstWithProp;
+  }
+};
+
+messageActions.connect = function(that, msg) {
+  if (msg.success) {
+    that.loggedIn = true;
+    that.loginDeferred.resolve();
+  } else {
+    that.loginDeferred.reject();
+  }
+};
+
+messageActions.character = function(that, msg) {
+  that.characters.push(msg.name);
+};
+
+messageActions.item = function(that, msg) {
+    if (that.meters[msg.id] !== undefined) {
+      msg.meter = that.meters[msg.id];
+      delete that.meters[msg.id];
+    }
+    that.items.push(msg);
+};
+
+messageActions.destroy = function(that, msg) {
+  that.items = that.items.filter(function(item) {
+    return item.id !== msg.id;
+  });
+};
+
+messageActions.attr = function(that, msg) {
+  that.attrs = msg.attrs;
+};
+
+messageActions.meter = function(that, msg) {
+  var item = this.findFirstWithProp(that.items, 'id', msg.id);
+  if (item !== undefined) {
+    item.meter = msg.meter;
+  } else {
+    that.meters[msg.id] = msg.meter;
+  }
+};
+
+messageActions.mchat = function(that, msg) {
+  that.chats.push({
+    id: msg.id,
+    name: msg.name
+  });
+};
+
+messageActions.pchat = function(that, msg) {
+  that.chats.push({
+    id: msg.id,
+    name: 'Party'
+  });
+};
+
+messageActions.pmchat = function(that, msg) {
+  that.chats.push({
+    id: msg.id,
+    name: msg.other
+  });
+};
+
+messageActions.pchatrm = function(that, msg) {
+  that.chats = that.chats.filter(function(chat) {
+    return chat.id !== msg.id;
+  });
+};
+
+messageActions.msg = function(that, msg) {
+  (that.msgs[msg.chat] = that.msgs[msg.chat] || []).push({
+    from: msg.from,
+    text: msg.text
+  });
+};
+
+messageActions.player = function(that, msg) {
+  that.players.push(msg.id);
+};
+
+messageActions.buddy = function(that, msg) {
+  that.buddies[msg.id] = msg.name;
+};
+
+messageActions.pgob = function(that, msg) {
+  that.pgob = msg.id;
+};
+
+messageActions.gobrem = function(that, msg) {
+  that.players = that.players.filter(function(playerId) {
+    return playerId !== msg.id;
+  });
+  delete that.buddies[msg.id];
+};
+
+messageActions.enc = function(that, msg) {
+  that.enc = msg.enc;
+};
+
+messageActions.exp = function(that, msg) {
+  that.exp = msg.exp;
+};
+
+messageActions.time = function(that, msg) {
+  that.tm = msg.time;
+  that.epoch = msg.epoch;
+  if (!msg.inc) {
+    that.lastrep = 0;
+  }
+};
+
+messageActions.kinadd = messageActions.kinupd = function(that, msg) {
+  that.kins[msg.id] = {
+    name: msg.name,
+    online: msg.online
+  };
+};
+
+messageActions.kinchst = function(that, msg) {
+  if (msg.id in that.kins) {
+    that.kins[msg.id].online = msg.online;
+  }
+};
+
+messageActions.kinrm = function(that, msg) {
+  delete that.kins[msg.id];
+};
+
+module.exports = messageActions;
