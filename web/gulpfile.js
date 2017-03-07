422464561b276145d37d6d0cc76cd7cf36365b2e
pil-a <pil-a@yandex.ru>
2017-03-07 17:06:35 +0300

    feature/glyphicons

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -7,6 +7,9 @@ var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
 var ngAnnotate = require('browserify-ngannotate');
 
+var path = require('path');
+var fse = require('fs-extra');
+
 function bundleScript(watch) {
   var b = browserify({
     entries: ['./client/scripts/app.js'],
@@ -15,7 +18,32 @@ function bundleScript(watch) {
     packageCache: {}
   });
   b.transform(ngAnnotate);
-  b.transform('browserify-css', { global: true });
+  b.transform('browserify-css', {
+        global: true,
+        processRelativeUrl: function(relativeUrl) {
+          var stripQueryStringAndHashFromPath = function(url) {
+              return url.split('?')[0].split('#')[0];
+          };
+          var relativePath = stripQueryStringAndHashFromPath(relativeUrl);
+          var queryStringAndHash = relativeUrl.substring(relativePath.length);
+          var rootDir = path.resolve(process.cwd());
+
+          var prefix = 'node_modules';
+          var indexOfPrefix = relativePath.indexOf(prefix); // 'relativePath' like '../../../node_modules/bootstrap..'
+          if (indexOfPrefix >= 0) {
+            relativePath = relativePath.substring(indexOfPrefix); // 'relativePath' now 'node_modules/bootstrap..'
+            var vendorPath = path.join('vendor', relativePath.substring(prefix.length)); // 'vendorPath' now 'vendor/bootstrap..'
+            var source = path.join(rootDir, relativePath);
+            var target = path.join(rootDir, 'client', vendorPath); // put 'vendor/bootstrap..' in 'client' folder
+
+            fse.copySync(source, target);
+
+            return vendorPath + queryStringAndHash;
+          }
+          return relativeUrl;
+        }
+    })
+
   b.plugin('minifyify', { map: 'bundle.js.map', output: './client/bundle.js.map' });
   if (watch) {
     b.plugin(watchify);
